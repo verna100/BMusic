a8421ffbc7dc332245d00c05eb81c10bd1c58cac
Vernaliz Gonzalez <vernaliz@Vernalizs-MacBook-Pro.local>
2019-09-03 12:56:28 -0400

    1st commit

---
+++ Personal page/assets/server.js
@@ -0,0 +1,14 @@
+  /* Load the HTTP library */
+  var http = require("http");
+
+//   /* Create an HTTP server to handle responses */
+
+  http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }).listen(8888);
+
+var client_id = '937149dbdba441118e95164689d25c16'; // Your client id
+var client_secret = 'dba383e293b4b708d5188a8e32bbcf8'; // Your secret
+var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
